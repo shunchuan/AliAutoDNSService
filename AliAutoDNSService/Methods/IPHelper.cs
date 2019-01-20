588eb55aad7eb7cdeb443ac2f3f6ec5a9bb89ffb
shunchuan <1289158751@qq.com>
2019-01-20 21:53:23 +0800

    fix:解决IP获取不稳定的BUG，使用www.net.cn获取ip地址

--- AliAutoDNSService/Methods/IPHelper.cs
+++ AliAutoDNSService/Methods/IPHelper.cs
@@ -37,8 +37,10 @@ namespace AliAutoDNSService.Methods
                 client.Encoding = System.Text.Encoding.Default;
                 client.Headers.Add("User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36");
                 client.Headers.Add("X-Requested-With: XMLHttpRequest");
-                string response = client.DownloadString("https://www.baidu.com/s?wd=ip");//百度
-                string myReg = "(?<=fk=\")(\\d|\\.)+";
+                //string response = client.DownloadString("https://www.baidu.com/s?wd=ip");//百度
+                //string myReg = "(?<=fk=\")(\\d|\\.)+";
+                string response = client.DownloadString("http://www.net.cn/static/customercare/yourip.asp");//百度
+                string myReg = @"((?:(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))\.){3}(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d))))";
                 Match mc = Regex.Match(response, myReg, RegexOptions.Singleline);
                 if (mc.Success && mc.Groups.Count > 1)
                 {
@@ -57,7 +59,5 @@ namespace AliAutoDNSService.Methods
                 return null;
             }
         }
-
-        
     }
 }
